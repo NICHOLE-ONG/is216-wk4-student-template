3c26c779db612954351672d58514bde8af47b020
Nichole Ong <nichole.ong.2025@computing.smu.edu.sg>
2026-09-06 21:39:04 +0800

    solved

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,19 +1,17 @@
 <script setup>
-    import { ref } from 'vue'
-
-    // Add code here
-    // challenge: use array  
-    
+import { ref } from 'vue'
+const message = ref('Choose your favourite fruit:')
+const fruit = ref(['apple', 'orange', 'peach'])
+// Add code here
+// challenge: use array
 </script>
 
 <template>
-    {{ message }} 
-   
-    <label for=""> <input type="radio" name="fruit"> {{ fruit1 }} </label> 
-    <label for=""> <input type="radio" name="fruit"> {{ fruit2 }} </label> 
-    <label for=""><input type="radio" name="fruit"> {{ fruit3 }} </label> 
-    
+  {{ message }}
+
+  <label for=""> <input type="radio" name="fruit" /> {{ fruit[0] }} </label>
+  <label for=""> <input type="radio" name="fruit" /> {{ fruit[1] }} </label>
+  <label for=""><input type="radio" name="fruit" /> {{ fruit[2] }} </label>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
